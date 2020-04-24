5238e5662278a231d169e6bae3fe28766404c3a8
RyuichiMatsuda <54462227+RyuichiMatsuda@users.noreply.github.com>
2020-04-24 18:20:14 +0900

    tocyuu

--- index.js
+++ index.js
@@ -3,14 +3,21 @@
         let num1 = $("#box1").val();
         let num2 = $("#box2").val();
         let total = 0
-        $("#lbl_total").css('font-size', '20px')
 
         if (!num1 || !num2) {
+
+
             $("#lbl_total").text("は?");
-            $("#lbl_total").css('font-size', '300px')
+            $("#lbl_total").animate({
+                'font-size':'300px'
+            },500
+            )
         } else {
             total = parseInt(num1) + parseInt(num2)
             $("#lbl_total").text(total);
+            $("#lbl_total").animate({
+                'font-size': '20px'
+            }, 500)
             comparison(total);
         }
     });
@@ -29,7 +36,7 @@ function comparison(total) {
         } else if (td == total) {
             $(element).animate(
                 {
-                    'opacity':0.9,
+                    'opacity': 0.9,
                     'backgroundColor': '#ffffe0'
                 }, 500
             );
@@ -38,7 +45,7 @@ function comparison(total) {
                 {
                     'opacity': 0.8,
                     'backgroundColor': '#ff6347'
-                }, 700
+                }, 800
             );
         }
     });
