eafcae55f9efbb809f3df4cff2aef3fffb18b9ea
RyuichiMatsuda <54462227+RyuichiMatsuda@users.noreply.github.com>
2020-04-24 14:23:42 +0900

    bootstrap apply
    bootstrap軽く適用してみた

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-$(function () {
+﻿$(function () {
     $("#total_sum").on('click', function () {
         let num1 = $("#box1").val();
         let num2 = $("#box2").val();
         let total = 0
-        total = parseInt(num1) + parseInt(num2)
-        $("#lbl_total").text(total);
-        comparison(total);
+        if (!num1 || !num2) {
+            $("#lbl_total").text("は?");
+            $("#lbl_total").css('font-size', '300px')
+        } else {
+            total = parseInt(num1) + parseInt(num2)
+            $("#lbl_total").text(total);
+            comparison(total);
+        }
     });
 });
 
@@ -13,11 +18,12 @@ function comparison(total) {
     $(".num_all").each(function (index, element) {
         let td = $(element).text();
         if (td < total) {
-            $(element).css('background-color', 'gray');
+            $(element).css('background-color', 'white');
         } else if (td == total) {
             $(element).css('background-color', 'yellow');
         } else if (td > total) {
             $(element).css('background-color', 'red');
         }
     });
+
 }
